6f36cc90444487bfe9cf0a75c76a242727d9f71e
Negin Zahedi <zahedi.negin@gmail.com>
2022-08-06 18:47:25 -0400

    Initial Commit

---
+++ SwiftUI-List/SwiftUI_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_ListApp.swift
+//  SwiftUI-List
+//
+//  Created by Negin Zahedi on 2022-08-06.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
