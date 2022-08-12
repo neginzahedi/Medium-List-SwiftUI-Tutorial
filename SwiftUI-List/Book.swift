a69123c24466f1fa052141bc343984ef91fe2529
Negin Zahedi <zahedi.negin@gmail.com>
2022-08-11 20:59:45 -0400

    Book structor created with some attributes

---
+++ SwiftUI-List/Book.swift
@@ -0,0 +1,17 @@
+//
+//  Book.swift
+//  SwiftUI-List
+//
+//  Created by Negin Zahedi on 2022-08-11.
+//
+
+import Foundation
+
+struct Book: Identifiable {
+    let id = UUID()
+    let cover: String
+    let name: String
+    let author: String
+    let publish_date: String
+    let summary: String
+}
